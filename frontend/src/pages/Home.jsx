f6b2845526319af3fe857a456ce935ef76ecb7c5
nottejas <padmakartejas24@gmail.com>
2025-05-15 13:48:16 +0530

    handling searchign techniques

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,9 +33,23 @@ function Home() {
     fetchApiKeyAndMovies();
   }, []);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
+    if(!searchQuery.trim()) return
+    if (loading) return
+
+    setLoading(true)
+    try{    
+        const searchResults = await searchMovies(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+    } catch (err) {
+        console.log(err);
+        setError("Failed to search movie...")
+    } finally {
+        setLoading(false)
+    }
+    setSearchQuery("");
   };
 
   return (
